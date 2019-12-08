f42548c34bc067391f6936000793f735b2e115db
Boktae <bok7370@gmail.com>
2019-12-08 14:44:59 +0900

    Updates

---
+++ precache-manifest.34097529af36c82d435bb718a50056d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b7492dd8641e78594847bb2069ed85",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d57cd02f2664f64174c8",
+    "url": "/movie_app_2019/static/css/main.e931a765.chunk.css"
+  },
+  {
+    "revision": "5936d38bf99b208b6fb5",
+    "url": "/movie_app_2019/static/js/2.5bfc5e0f.chunk.js"
+  },
+  {
+    "revision": "d57cd02f2664f64174c8",
+    "url": "/movie_app_2019/static/js/main.9942ef04.chunk.js"
+  },
+  {
+    "revision": "58e37996626726930b78",
+    "url": "/movie_app_2019/static/js/runtime-main.3cf556df.js"
+  }
+]);
